6d084811861464d893e4672d221141c77ddcda0b
rahulatlamp <rahulrajan@lampros.tech>
2022-08-06 16:45:08 +0530

    Added Piano

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Composer from "./piano/Composer";
+import "./player.css";
 
 const Player = () => {
   return (
-    <div className="player-main">
-      <div>player</div>
+    <div className="player">
+      <div className="player-main">
+        <span className="piano-label">PIANO</span>
+        <div className="piano-holder">
+          <div className="piano-main">
+            <Composer />
+          </div>
+          <div className="piano-control">Controls</div>
+        </div>
+      </div>
     </div>
   );
 };
