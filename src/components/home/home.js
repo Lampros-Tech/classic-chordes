178d38b970d875ddf111b6114ca5bf8aad3a27a8
Adithya Srinivasan <adithyas.lamprostech@gmail.com>
2022-08-06 16:40:07 +0530

    Design Homepage and Navbar

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./home.css";
+import HomeLogo from "../../images/homelogo.png";
+import HomeText from "../../images/hometext.png";
+import HomeButton from "../../images/homebutton.png";
+import HomeImg from "../../images/homeimg.png";
 
 const Home = () => {
   return (
-    <div>
-      <div>Home</div>
+    <div className="home">
+      <div className="outerbg">
+        <div className="innerbg">
+          <div className="home-left">
+            <div className="home-logo">Classic Chords</div>
+
+            <div className="home-content">
+              Unleash The Artist In You, & Make Your Own Web3 Music
+            </div>
+
+            <button className="home-button">Play Now</button>
+          </div>
+          <div className="home-right">
+            <img className="home-img" src={HomeImg} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,89 +2,12 @@ import React, { useState, useEffect } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
-import { debounce } from "./helper";
 import "./navbar.scss";
-
-const networks = {
-  polygon: {
-    chainId: `0x${Number(137).toString(16)}`,
-    chainName: "Polygon Mainnet",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
-    },
-    rpcUrls: ["https://polygon-rpc.com/"],
-    blockExplorerUrls: ["https://polygonscan.com/"],
-  },
-  // bsc: {
-  //   chainId: `0x${Number(56).toString(16)}`,
-  //   chainName: "Binance Smart Chain Mainnet",
-  //   nativeCurrency: {
-  //     name: "Binance Chain Native Token",
-  //     symbol: "BNB",
-  //     decimals: 18,
-  //   },
-  //   rpcUrls: [
-  //     "https://bsc-dataseed1.binance.org",
-  //     "https://bsc-dataseed2.binance.org",
-  //     "https://bsc-dataseed3.binance.org",
-  //     "https://bsc-dataseed4.binance.org",
-  //     "https://bsc-dataseed1.defibit.io",
-  //     "https://bsc-dataseed2.defibit.io",
-  //     "https://bsc-dataseed3.defibit.io",
-  //     "https://bsc-dataseed4.defibit.io",
-  //     "https://bsc-dataseed1.ninicoin.io",
-  //     "https://bsc-dataseed2.ninicoin.io",
-  //     "https://bsc-dataseed3.ninicoin.io",
-  //     "https://bsc-dataseed4.ninicoin.io",
-  //     "wss://bsc-ws-node.nariox.org",
-  //   ],
-  //   blockExplorerUrls: ["https://bscscan.com"],
-  // },
-};
-
-const changeNetwork = async ({ networkName, setError }) => {
-  try {
-    if (!window.ethereum) throw new Error("No crypto wallet found");
-    await window.ethereum.request({
-      method: "wallet_addEthereumChain",
-      params: [
-        {
-          ...networks[networkName],
-        },
-      ],
-    });
-  } catch (err) {
-    setError(err.message);
-  }
-};
 
 const Navbar = () => {
   const [error, setError] = useState();
 
-  const handleNetworkSwitch = async (networkName) => {
-    setError();
-    await changeNetwork({ networkName, setError });
-  };
-
-  const networkChanged = (chainId) => {
-    console.log({ chainId });
-  };
-
-  useEffect(() => {
-    window.ethereum.on("chainChanged", networkChanged);
-
-    return () => {
-      window.ethereum.removeListener("chainChanged", networkChanged);
-    };
-  }, []);
-
   const [open, setOpen] = useState(false);
-
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-
-  const [visible, setVisible] = useState(true);
 
   const handleClick = () => {
     setOpen(!open);
@@ -94,32 +17,8 @@ const Navbar = () => {
     setOpen(false);
   };
 
-  const [newButton, setNewButton] = useState({ hidden: false });
-  const onClick = () => {
-    setNewButton({ hidden: true });
-  };
-
-  const handleScroll = debounce(() => {
-    const currentScrollPos = window.pageYOffset;
-
-    setVisible(
-      (prevScrollPos > currentScrollPos &&
-        prevScrollPos - currentScrollPos > 70) ||
-        currentScrollPos < 10
-    );
-
-    setPrevScrollPos(currentScrollPos);
-    // timer set to 100 milliseconds:
-  }, 100);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos, visible, handleScroll]);
-
   return (
-    <nav className="navbar" style={{ top: visible ? "0" : "-100px" }}>
+    <nav className="navbar">
       <Link to="/" className="nav-logo">
         <img style={{ width: "250px", height: "80px" }} src={logo} />
       </Link>
@@ -134,17 +33,17 @@ const Navbar = () => {
         </li>
         <li className="nav-item">
           <Link to="/player" className="nav-link" onClick={closeMenu}>
-            <div className="navtextstyle">player</div>
+            <div className="navtextstyle">Player</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link to="/explore" className="nav-link" onClick={closeMenu}>
-            <div className="navtextstyle">explore</div>
+            <div className="navtextstyle">Explore</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link to="/streaming" className="nav-link" onClick={closeMenu}>
-            <div className="navtextstyle">stream</div>
+            <div className="navtextstyle">Stream</div>
           </Link>
         </li>
         <li className="nav-item"></li>
